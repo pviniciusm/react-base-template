b79573472ea0ad967f515cf011435c9e9a4404f6
Paulo <pvinics@gmail.com>
2024-02-20 14:10:23 -0300

    fix: set css baseline to inside of theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { Provider } from "react-redux";
 import { persistedStore, store } from "./config/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { Root } from "./Root";
-import { CssBaseline } from "@mui/material";
 
 function App() {
     return (
         <>
             <Provider store={store}>
                 <PersistGate persistor={persistedStore}>
-                    <CssBaseline />
                     <Root />
                 </PersistGate>
             </Provider>
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes/routes";
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { defaultTheme } from "./config/themes/default.theme";
+// import { darkTheme } from "./config/themes/dark.theme";
 
 export const Root = () => {
     return (
         <ThemeProvider theme={defaultTheme}>
+            <CssBaseline />
             <RouterProvider router={routes} />
         </ThemeProvider>
     );
